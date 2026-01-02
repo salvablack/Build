80a286a8b291a431b22deae0711e4ebb584144c4
Salva Rosales <147666124+salvablack@users.noreply.github.com>
2026-01-01 18:57:49 -0600

    Update redrock.py

--- redrock.py
+++ redrock.py
@@ -21,7 +21,7 @@ from csv_engine import process_csv
 # ================= CONFIG =================
 st.set_page_config(page_title="R E D R O C K", layout="wide")
 st.title("🟥 R E D R O C K")
-st.caption("Data inspection • filtros • métricas • agrupaciones • gráficos y más")
+st.caption("Data inspection • filtros • métricas • agrupaciones • gráficos")
 
 # ================= STATE =================
 if "filters" not in st.session_state:
@@ -264,5 +264,3 @@ if st.button("⬇ Exportar PDF profesional"):
         "redrock_reporte.pdf",
         mime="application/pdf"
     )
-
-
